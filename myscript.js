5cb33f064b03494a0116a6a7e112a14f4c2659f8
giomessina <messgiorgio@gmail.com>
2024-07-18 17:22:47 +0200

    new test
    boh non ho capito benissimo cosa stia facendo ma a volte funziona la preview.
    
    Devo ancora capire:
    - come mai il * di css non viene caricato ma viene solo letto da head di index
    - come mai non mi prende il

--- myscript.js
+++ myscript.js
@@ -1,18 +1,15 @@
 function showParagraph(contentId) {
-    // Hide all paragraphs including the default one
-    document.querySelectorAll('#content > p').forEach(p => p.classList.add('hidden'));
+    // Hide all divs including the default one
+    document.querySelectorAll('#content > div').forEach(div => div.classList.add('hidden'));
     
-    // Show the selected paragraph
+    // Show the selected div
     document.getElementById(contentId).classList.remove('hidden');
 }
 
 function resetPage() {
-    // Hide all paragraphs
-    document.querySelectorAll('#content > p').forEach(p => p.classList.add('hidden'));
+    // Hide all divs
+    document.querySelectorAll('#content > div').forEach(div => div.classList.add('hidden'));
     
     // Show the default content
     document.getElementById('content0').classList.remove('hidden');
 }
-
-// The default content (content0) is visible by default,
-// so we don't need to call showParagraph() on page load
