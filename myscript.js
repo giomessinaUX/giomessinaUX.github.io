1f1c1405881d26071025b41702997d3cca0d6a4d
giomessina <messgiorgio@gmail.com>
2024-07-20 08:57:55 +0200

    THE FUCK IT'S UNWORKING
    non è che non è working è che è successfully failing

--- myscript.js
+++
@@ -1,8 +0,0 @@
-function toggleParagraph(contentId) {
-    const content = document.getElementById(contentId);
-    if (content.classList.contains('hidden')) {
-        content.classList.remove('hidden');
-    } else {
-        content.classList.add('hidden');
-    }
-}
